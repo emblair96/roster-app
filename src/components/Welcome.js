76e1b18ac9cd317d64e17eaa55f9b0c43597f485
Emily Blair <emilyblair96@gmail.com>
2021-02-06 14:06:15 -0600

    Add greeting display based on time of day

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,14 @@
 function Welcome() {
+  const currentHour = new Date().getHours();
+  const message  = currentHour < 5 ? "Why are you still up"
+                  : currentHour < 12 ? "Good morning"
+                  : currentHour < 18 ? "Good afternoon"
+                  : "Good Evening";
   return (
-    <h2>Welcome Cardinals Fans!</h2>
+  <>
+    <p>{message}</p>
+    <h2>Welcome Astros Fans!</h2>
+  </>
   )
 }
 
