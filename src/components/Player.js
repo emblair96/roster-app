46cf634b97b3a2705c07195240162889e3c56be7
Emily Blair <emilyblair96@gmail.com>
2021-02-06 14:08:07 -0600

    Empty hobbies render appropriate message

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,13 +1,13 @@
 function Player(props) {
-  const playerHobbies = props.hobbies.map(hobbie => {
-    return <li>{hobbie}</li>
-  });
+  const playerHobbies = props.hobbies.length 
+  ? props.hobbies.map(hobbie => { return <li>{hobbie}</li> })
+  : <p>No hobbies</p>
+
   console.log(props.hobbies)
   return (
     <div>
-      <h3>{props.firstName} || {props.jerseyNumber}</h3>
+      <h3>{props.firstName} #{props.jerseyNumber}</h3>
       <ul>{playerHobbies}</ul>
-        
     </div>
   )
 }
